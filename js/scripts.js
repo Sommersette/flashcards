1456fb41706b60e2cf25d5f8c265ee579c6b50bf
Trevor Fast and Tyler Stephenson <trevor@>
2017-03-21 16:57:52 -0700

    added javascript rules and removed background image from covering the answer whcih included lots of css

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(function(){
-  $()
+  $(".card").click(function(){
+    $(this).toggleClass("flip");
+    $(this).toggleClass("card-img");
+    $("span", this).toggleClass("backside");
+    $("p", this).toggleClass("backside");
+  });
+
+
 });
